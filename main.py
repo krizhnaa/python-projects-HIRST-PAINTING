a600bce0007caf1e428efcb356748640d6f15ed6
krizhnaa <krizhnaprem@gmail.com>
2023-11-12 13:15:46 +0530

    position and color filling completed

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ miya.pensize(15)
 
 color_list = [(236, 35, 108), (146, 28, 66), (239, 75, 35), (230, 237, 232), (7, 148, 94), (220, 171, 45), (182, 159, 48), (44, 191, 232), (28, 126, 194), (253, 223, 0), (125, 192, 79), (84, 28, 91), (245, 219, 50), (179, 40, 98), (42, 169, 116), (209, 131, 165), (205, 56, 35)]
 
+for color in color_list:
+    miya.pendown()
+    miya.forward(1)
+    miya.penup()
+    miya.color(color)
+    miya.forward(100)
 
 screen = Screen()
 screen.screensize(100, 100)
