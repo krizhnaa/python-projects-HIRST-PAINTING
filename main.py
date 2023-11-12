24da9c9a8e957c9e64d405c65800fc1505255e00
krizhnaa <krizhnaprem@gmail.com>
2023-11-12 13:33:35 +0530

    kinda of done with the project, have to implement termination when it reaches certain stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import colorgram
 from turtle import Turtle, Screen, colormode
+import random
 
 # colors = colorgram.extract('image.jpg',20)
 # rgb_color = []
@@ -18,16 +19,32 @@ miya.shape('arrow')
 miya.pensize(15)
 
 
-
 color_list = [(236, 35, 108), (146, 28, 66), (239, 75, 35), (230, 237, 232), (7, 148, 94), (220, 171, 45), (182, 159, 48), (44, 191, 232), (28, 126, 194), (253, 223, 0), (125, 192, 79), (84, 28, 91), (245, 219, 50), (179, 40, 98), (42, 169, 116), (209, 131, 165), (205, 56, 35)]
 
-for color in color_list:
-    miya.pendown()
-    miya.forward(1)
+miya.penup()
+miya.setheading(225)
+miya.forward(250)
+miya.setheading(0)
+
+for _ in range(0, 10):
+    miya.dot(20, random.choice(color_list))
     miya.penup()
-    miya.color(color)
-    miya.forward(100)
+    miya.forward(50)
+
+while True:
+    miya.setheading(90)
+    miya.forward(50)
+    miya.setheading(180)
+    miya.forward(500)
+    miya.setheading(0)
+
+    for _ in range(0,10):
+        miya.dot(20, random.choice(color_list))
+        miya.penup()
+        miya.forward(50)
+
+
 
 screen = Screen()
-screen.screensize(100, 100)
+screen.screensize(canvwidth=100, canvheight=100)
 screen.exitonclick()
