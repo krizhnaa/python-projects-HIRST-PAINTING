079399fd48fa16711d7cf6ddcfe3eee12df016e3
krizhnaa <krizhnaprem@gmail.com>
2023-11-12 12:46:06 +0530

    Extracted colors from an image using colorgram package

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import colorgram
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+colors = colorgram.extract('image.jpg',20)
+rgb_color = []
 
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgb = (r, g, b)
+    rgb_color.append(rgb)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(rgb_color)
+color_list = [(236, 35, 108), (146, 28, 66), (239, 75, 35), (230, 237, 232), (7, 148, 94), (220, 171, 45), (182, 159, 48), (44, 191, 232), (28, 126, 194), (253, 223, 0), (125, 192, 79), (84, 28, 91), (245, 219, 50), (179, 40, 98), (42, 169, 116), (209, 131, 165), (205, 56, 35)]
